48ad3767bb7bd0e9c117505eff6074e9391f4498
Dmytro <60152142+TsurkanDmytro@users.noreply.github.com>
2020-03-04 22:24:24 +0200

    Add files via upload

--- HomeWork8/HomeWork8.js
+++ HomeWork8/HomeWork8.js
@@ -1,112 +1,105 @@
-
-
-   /* Task 1 У стдентов должны быть следующие свойства: university, course, fullName, они передаются при создании студента
-    (в конструктор). */
+/* В этом задании вам необходимо создать класс Student и разрабатывать методы внутри этого класса. */
+/* Task 1 У стдентов должны быть следующие свойства: university, course, fullName, они передаются при создании студента(в конструктор). */
 
 class Student {
-    constructor(university, course, fullName) {
-      this.university = university;
-      this.course = course;
-      this.fullName = fullName;
-    }
-    _marks = [5, 4, 4, 5];
-
-
-   /*  Task 2 Создайте метод this.getInfo() -> "Студент 1го курса Высшей Школы Мошенничества г.Одесса, Остап Родоманський Бендер",
-    метод возвращает совокупную информацию о курсе, учебном заведении и имени студента. */
-    getInfo() {
-      return `Студент ${this.course}го курсу, ${this.university}, ${this.fullName}`;
-    }
-
-
-    /* Task 3 Создайте геттер оценок this.marks, который возвращает массив оценок студента [5, 4, 4, 5] */
-    get studentMarks() {
-      return this._marks;
-    }
- /*   Task 4 Создайте сеттер оценок this.marks = 5, который позволяет поставить оценку студенту. После того,
-     как оценка поставлена, геттер должен вернуть массив this.marks -> [5, 4, 4, 5, 5] */
-    set studentMarks(value) {
-      if (this._marks != null) {
-        this._marks.push(value);
-      }
-    }
-
-
-    /*  Task 5 Создайте метод получения среднего балла this.getAverageMark() -> 4.6 */
-    getAverageMark() {
-      let sum = 0;
-      if (this._marks != null) {
-        for (let i = 0; i < this._marks.length; i++) {
-          sum += this._marks[i];
-        }
-      } else {
-        return null;
-      }
-      return sum / this._marks.length;
-    }
-
-
-   /* Task 6 оздайте метод this.dismiss, который "исключит" студента. После вызова этого метода – ставить студенту оценки
-     и получать их больше нельзя. (Никаких ошибок, просто возвращается всегда null вместо массива оценок) */
-    dismiss() {
-      this._marks = null;
-      return console.log(`Cтудент відрахований!`);
-    }
-
-
-   /*  Task 7 Создайте метод this.recover, который позволит восстановить студента */
-    recover() {
-      this._marks = [5, 4, 4, 5];
-      return console.log(`Cтудент зачислений повторно!`);
-    }
+  marks = [5, 4, 4, 5];
+  constructor(university, course, fullName)
+  {this.university = university;
+    this.course = course;
+    this.fullName = fullName;
   }
-  
-  let studentOne = new Student("LP", 3, "Ivan Ivanovych Ivanov");
-  console.log(studentOne.getInfo());
-  console.log(studentOne.studentMarks);
-  studentOne.studentMarks = 5;
-  console.log(studentOne.studentMarks);
-  console.log(studentOne.getAverageMark());
-  studentOne.dismiss();
-  console.log(studentOne.studentMarks);
-  studentOne.recover();
-  console.log(studentOne.getAverageMark());
-  
 
-/*      Level Advanced    */
-  
-   /*   Task 1 Создайте новый класс BudgetStudent, который полностью наследует класс Student  */
-  class BudgetStudent extends Student {
-    constructor(university, course, fullName) {
+
+/* Task 2 
+Создайте метод this.getInfo() -> "Студент 1го курса Высшей Школы Мошенничества г.Одесса, Остап Родоманський Бендер", 
+метод возвращает совокупную информацию о курсе, учебном заведении и имени студента. */
+
+get getInfo() {
+  return console.log ('Студент: ' + this.fullName + '; Університет: ' + this.university + '; Курс: ' + this.course + ';');
+}
+
+/* Task 3 
+Создайте геттер оценок this.marks, который возвращает массив оценок студента [5, 4, 4, 5] */
+
+set studentMarks(number) {
+  if (this.marks === null) {
+      return null;
+  } else {
+      this.marks.push(number);
+  }
+}
+
+/* Task 4 
+Создайте сеттер оценок this.marks = 5, который позволяет поставить оценку студенту. После того, как оценка поставлена, 
+геттер должен вернуть массив this.marks -> [5, 4, 4, 5, 5]*/
+
+get getMarks() {
+  return console.log(this.marks);
+}
+
+/* Task 5
+Создайте метод получения среднего балла this.getAverageMark() -> 4.6 */
+
+getAverageMark() {
+  if (this.marks == null) {
+      return null;
+  } else {
+      return this.marks.reduce((prevValue, value) => {
+          prevValue += value;
+          return prevValue;
+      }, 0) / this.marks.length;
+  }
+
+}
+
+/* Task 6 
+создайте метод this.dismiss, который "исключит" студента. После вызова этого метода – ставить студенту оценки
+и получать их больше нельзя. (Никаких ошибок, просто возвращается всегда null вместо массива оценок)*/
+
+dismiss() {
+  this.marks = null;
+  return console.log('студента відраховано!');
+}
+
+/* Task 7 
+Создайте метод this.recover, который позволит восстановить студента*/
+
+recover() {
+  this.marks = [5, 4, 4, 5];
+  return console.log('студента відновлено!');
+}
+}
+let dmytro = new Student('LNU Franko', 3, 'Dmytro Tsurkan');
+dmytro.getInfo;
+dmytro.getMarks;
+dmytro.dismiss();
+dmytro.getMarks;
+dmytro.setMarks = 5;
+console.log(dmytro.getAverageMark());
+dmytro.recover();
+
+
+/* Advanced */
+
+class BudgetStudent extends Student {
+  constructor(university, course, fullName) {
       super(university, course, fullName);
-
- /*  Task 2 Бюджетный студент может получать стипендию с помощью метода this.getScholarship. Получение стипендии
-    сопровождается выводом информации в консоль: Вы получили 1400 грн. стипендии */
-    getScholarship() {
-        const scholarship = 1400;
-        if (this.getAverageMark() > 4) {
-          return console.log(`Ви отримали ${scholarship}грн. стипендії`);
-        }
-
-   /*  Task 3 Метод получения стипендии автоматически вызывается каждые 30 секунд после создания объекта.
-      Подсказка: вызывайте его в constructor */
-      setInterval(() => {
-        this.getScholarship();
-      }, 30000);
-    }
-
-
-  
-      /* Task 5 Если студент исключен, он не получает стипендию (думаю это было и так очевидно :) ) */
-      else if (this._marks === null) {
-        return console.log(`Ви не маєте стипендії`);
-      } else {
-        console.log(`Ваш бал занизький для отримання стипендії`);
+      if (parseFloat(this.getAverageMark()) > 4) {
+          setInterval(() => {
+              this.getScholarship();
+          }, 30000);
       }
-    }
   }
-  let studentTwo = new BudgetStudent("LP", 2, "Bohdan Ivanovych Ivanov");
-  console.log(studentTwo.getInfo());
-  console.log(studentTwo.studentMarks);
-  studentTwo.getScholarship();
-  
+  getScholarship() {
+      if (parseFloat(this.getAverageMark()) > 4){
+          return console.log('Ви отримали стипнедію 1250 грн.!');
+      } else if(this.marks === null) {
+          return console.log('Ви не можете отримати стипендію!');
+      }
+  }
+}
+
+let ivan = new BudgetStudent('Politechnic ', 4, 'Ivan Ivanov ');
+ivan.getInfo;
+ivan.getMarks;
+ivan.getScholarship();
