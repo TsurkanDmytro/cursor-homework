f7771730ccf2ccca57492c5c71d7aeedfd9e2ae4
Dmytro <60152142+TsurkanDmytro@users.noreply.github.com>
2020-03-08 23:56:45 +0200

    Add files via upload

---
+++ HomeWork9/HomeWork9.js
@@ -0,0 +1,42 @@
+let squares = document.querySelector('.bigSquare');
+function generateBlocks() {
+  for (let i = 0; i < 25; i++) {
+    let littleSquare = document.createElement('div');
+    littleSquare.className = 'blocks';
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    littleSquare.style.width = '50px';
+    littleSquare.style.height = '50px';
+    littleSquare.style.background = `rgb(${r},${g},${b})`;
+    squares.append(littleSquare);
+  }
+}
+
+function removeBlocks() {
+  let allBlocks = document.querySelectorAll('.blocks');
+  for (let i = 0; i < allBlocks.length; i++) {
+    allBlocks[i].remove();
+  }
+}
+
+
+
+/* function setColors() {
+  document.querySelectorAll('.blocks').forEach(value => {
+      value.style.transition = '1s';
+      value.style.backgroundColor = getRandomColor();
+
+  });
+ */
+
+function generateBlocksInterval() {
+  removeBlocks();
+  let interval = setInterval(() => {
+    removeBlocks();
+    generateBlocks();
+  }, 1000);
+  return interval;
+}
+
+generateBlocksInterval();
